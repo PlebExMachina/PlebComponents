47c8297aca3fe4cab77a652e48cd0bfcf0f4b537
Kenneth Willeford <plebexmachina@gmail.com>
2022-05-25 20:15:38 -0700

    Wrong approach for SnapToComponent, saving work

---
+++ Source/PlebComponents/SnapToComponent.cpp
@@ -0,0 +1,227 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SnapToComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Net/UnrealNetwork.h"
+
+namespace {
+	auto IsServer = [](UObject* o) -> bool {
+		return o->GetWorld()->GetAuthGameMode() != nullptr;
+	};
+};
+
+void USnapToComponent::GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(USnapToComponent, _SnapToActor);
+	DOREPLIFETIME(USnapToComponent, _SnapToComponent);
+	DOREPLIFETIME(USnapToComponent, _SnapToLocation);
+	DOREPLIFETIME(USnapToComponent, _FocusActors);
+	DOREPLIFETIME(USnapToComponent, bRotatePitch);
+	DOREPLIFETIME(USnapToComponent, bRotateYaw);
+	DOREPLIFETIME(USnapToComponent, bTranslateX);
+	DOREPLIFETIME(USnapToComponent, bTranslateY);
+	DOREPLIFETIME(USnapToComponent, bTranslateZ);
+	DOREPLIFETIME(USnapToComponent, fRotateSpeed);
+	DOREPLIFETIME(USnapToComponent, fTranslateSpeed);
+	DOREPLIFETIME(USnapToComponent, SnapMode);
+
+	// This is used so that replication can control ticking for late joins and etc.
+	DOREPLIFETIME(USnapToComponent, _bIsTicking);
+}
+
+// Sets default values for this component's properties
+USnapToComponent::USnapToComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+	SetIsReplicatedByDefault(true);
+
+	// Defaults
+	bRotatePitch = false;
+	bRotateYaw = true;
+	bTranslateX = true;
+	bTranslateY = true;
+	bTranslateZ = false;
+	fRotateSpeed = 600.f;
+	fTranslateSpeed = 200.f;
+	SnapMode = ESnapMode::Actor;
+
+	_SnapToActor = nullptr;
+	_SnapToComponent = nullptr;
+	_SnapToLocation = FVector::ZeroVector;
+	_FocusActors = {};
+
+	_bIsTicking = false;
+}
+
+
+const FVector USnapToComponent::_GetSnapToLocation(AActor* Actor)
+{
+	FVector FocusLocation = FVector::ZeroVector;
+	switch (SnapMode) {
+	case ESnapMode::Actor:
+		FocusLocation = _SnapToActor != nullptr ? _SnapToActor->GetActorLocation() : Actor->GetActorLocation();
+		break;
+	case ESnapMode::Component:
+		FocusLocation = _SnapToComponent != nullptr ? _SnapToComponent->GetComponentLocation() : Actor->GetActorLocation();
+		break;
+	case ESnapMode::Manual:
+		FocusLocation = _SnapToLocation;
+		break;
+	}
+
+	// If ignoring then set actor current value as final value.
+	if (!bTranslateX) {
+		FocusLocation.X = Actor->GetActorLocation().X;
+	}
+	if (!bTranslateY) {
+		FocusLocation.Y = Actor->GetActorLocation().Y;
+	}
+	if (!bTranslateZ) {
+		FocusLocation.Z = Actor->GetActorLocation().Z;
+	}
+
+	return FocusLocation;
+}
+
+const FRotator USnapToComponent::_GetSnapToRotation(AActor* Actor)
+{
+	FRotator FocusRotation = UKismetMathLibrary::FindLookAtRotation(Actor->GetActorLocation(), _GetSnapToLocation(Actor));
+
+	// If ignoring then set actor current value as final value.
+	if (!bRotatePitch) {
+		FocusRotation.Pitch = Actor->GetActorRotation().Pitch;
+	}
+	if (!bRotateYaw) {
+		FocusRotation.Yaw = Actor->GetActorRotation().Yaw;
+	}
+	return FocusRotation;
+}
+
+void USnapToComponent::OnRep_SetTick()
+{
+	SetComponentTickEnabled(_bIsTicking);
+}
+
+// Called every frame
+void USnapToComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	bool ShouldRotate = bRotatePitch || bRotateYaw;
+	bool ShouldTranslate = bTranslateX || bTranslateY || bTranslateZ;
+
+	// Event causing actors should be isolated as otherwise removing from the list of Actors (an expected behavior) would be unsafe.
+	TArray<AActor*> InterruptedActors = {};
+	TArray<AActor*> FinishedActors = {};
+
+	for (auto Actor : _FocusActors) {
+		bool InterruptionOccured = false;
+
+		// If disabled then treat it as if it already succeeded.
+		bool RotationFinished = !ShouldRotate || false;
+		bool TranslationFinished = !ShouldTranslate || false;
+
+		// interpolate rotation
+		if (ShouldRotate) {
+			FRotator InterpolatedRotator = UKismetMathLibrary::RInterpTo_Constant(Actor->GetActorRotation(), _GetSnapToRotation(Actor), DeltaTime, fRotateSpeed);
+			InterruptionOccured |= ! Actor->SetActorRotation(InterpolatedRotator);
+			RotationFinished = _GetSnapToRotation(Actor).Equals(Actor->GetActorRotation(),0.01f);
+		}
+
+		// interpolate location
+		if (ShouldTranslate) {
+			FVector InterpolatedVector = UKismetMathLibrary::VInterpTo_Constant(Actor->GetActorLocation(), _GetSnapToLocation(Actor), DeltaTime, fTranslateSpeed);
+			InterruptionOccured |= ! Actor->SetActorLocation(InterpolatedVector, true, nullptr, ETeleportType::TeleportPhysics);
+			TranslationFinished = _GetSnapToLocation(Actor).Equals(Actor->GetActorLocation(), 0.01f);
+		}
+
+		// Only track actors for events on server.
+		if (IsServer(this)) {
+			if (InterruptionOccured) {
+				InterruptedActors.Add(Actor);
+			}
+			if (!InterruptionOccured && RotationFinished && TranslationFinished) {
+				FinishedActors.Add(Actor);
+			}
+		}
+	}
+
+	// Only broadcasts events on server.
+	if (IsServer(this)) {
+		for (auto Actor : InterruptedActors) {
+			OnFocusInterrupted.Broadcast(this, Actor);
+		}
+
+		for (auto Actor : FinishedActors) {
+			OnFocusFinished.Broadcast(this, Actor);
+		}
+	}
+}
+
+void USnapToComponent::SetSnapToActor(AActor* FocusActor)
+{
+	if (IsServer(this)) {
+		_SnapToActor = FocusActor;
+	}
+}
+
+void USnapToComponent::SetSnapToComponent(USceneComponent* FocusComponent)
+{
+	if (IsServer(this)) {
+		_SnapToComponent = FocusComponent;
+	}
+}
+
+void USnapToComponent::SetSnapToLocation(const FVector& FocusLocation)
+{
+	if (IsServer(this)) {
+		_SnapToLocation = FocusLocation;
+	}
+}
+
+void USnapToComponent::AddFocusingActor(AActor* Actor)
+{
+	if (IsServer(this)) {
+		if ((_FocusActors.AddUnique(Actor) == INDEX_NONE) && IsComponentTickEnabled()) {
+			OnFocusStarted.Broadcast(this,Actor);
+		}
+		UE_LOG(LogTemp, Warning, TEXT("DEBUGGING %d"), _FocusActors.Num());
+	}
+}
+
+void USnapToComponent::ReleaseFocusingActor(AActor* Actor)
+{
+	if (IsServer(this)) {
+		if ((_FocusActors.RemoveSingle(Actor)) && IsComponentTickEnabled()) {
+			OnFocusInterrupted.Broadcast(this, Actor);
+		}
+	}
+}
+
+void USnapToComponent::BeginFocus()
+{
+	if (IsServer(this) && !_bIsTicking) {
+		_bIsTicking = true;
+		for (auto Actor : _FocusActors) {
+			OnFocusStarted.Broadcast(this, Actor);
+		}
+		OnRep_SetTick();
+	}
+}
+
+void USnapToComponent::EndFocus()
+{
+	if (IsServer(this) && _bIsTicking) {
+		_bIsTicking = false;
+		for (auto Actor : _FocusActors) {
+			OnFocusInterrupted.Broadcast(this, Actor);
+		}
+		OnRep_SetTick();
+	}
+}
+
---
+++ Source/PlebComponents/SnapToComponent.h
@@ -0,0 +1,125 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SnapToComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FSnapToComponentDelegate, class USnapToComponent*, Comp, AActor*, Actor);
+
+UENUM(BlueprintType)
+enum class ESnapMode : uint8 {
+	Actor,
+	Component,
+	Manual,
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PLEBCOMPONENTS_API USnapToComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USnapToComponent();
+
+	// Whether pitch should be taken into account when snapping.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+	bool bRotatePitch;
+
+	// Whether yaw should be taken into account when snapping.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+	bool bRotateYaw;
+
+	// Whether X position should be taken into account when snapping.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+	bool bTranslateX;
+
+	// Whether Y position should be taken into account when snapping.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+	bool bTranslateY;
+
+	// Whether Z position should be taken into account when snapping.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+	bool bTranslateZ;
+
+	// How quickly to snap rotation to the focus.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+	float fRotateSpeed;
+
+	// How quickly to snap translation to the focus.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+	float fTranslateSpeed;
+
+	// Determines what to use when snapping actors towards a target.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+	ESnapMode SnapMode;
+
+	// Event Fired when Focus begins or an Actor is added when focus has already started.
+	UPROPERTY(BlueprintAssignable)
+	FSnapToComponentDelegate OnFocusStarted;
+
+	// Event Fired an Actor has completed rotating / translating towards the focus. This can potentially fire per tick so use responsibly.
+	UPROPERTY(BlueprintAssignable)
+	FSnapToComponentDelegate OnFocusFinished;
+
+	// Event Fired when an actor is untracked when focus has already started or when translation / rotation has encountered an issue. 
+	UPROPERTY(BlueprintAssignable)
+	FSnapToComponentDelegate OnFocusInterrupted;
+
+protected:
+	UPROPERTY(Replicated)
+	AActor* _SnapToActor;
+
+	UPROPERTY(Replicated)
+	USceneComponent* _SnapToComponent;
+
+	UPROPERTY(Replicated)
+	FVector _SnapToLocation;
+
+	UPROPERTY(Replicated)
+	TArray<AActor*> _FocusActors;
+
+	UPROPERTY(ReplicatedUsing = "OnRep_SetTick")
+	bool _bIsTicking;
+
+	const FVector _GetSnapToLocation(AActor* Actor);
+
+	const FRotator _GetSnapToRotation(AActor* Actor);
+
+	UFUNCTION()
+	void OnRep_SetTick();
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	// Sets the Snap to reference. Only works on Server.
+	UFUNCTION(BlueprintCallable)
+	void SetSnapToActor(AActor* FocusActor);
+
+	// Sets the Snap to reference. Only works on Server.
+	UFUNCTION(BlueprintCallable)
+	void SetSnapToComponent(USceneComponent* FocusComponent);
+
+	// Sets the Snap to reference. Only works on Server.
+	UFUNCTION(BlueprintCallable)
+	void SetSnapToLocation(const FVector& FocusLocation);
+	
+	// Adds actor to Focus Array. Only works on Server.
+	UFUNCTION(BlueprintCallable)
+	void AddFocusingActor(AActor* Actor);
+
+	// Releases actor from Focus Array. Only works on Server.
+	UFUNCTION(BlueprintCallable)
+	void ReleaseFocusingActor(AActor* Actor);
+
+	// Begins focusing for actors in the array. Only works on Server.
+	UFUNCTION(BlueprintCallable)
+	void BeginFocus();
+
+	// Ends focusing for actors in the array. Only works on Server.
+	UFUNCTION(BlueprintCallable)
+	void EndFocus();
+};
